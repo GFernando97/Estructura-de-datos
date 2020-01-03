7cc0ee0d25510de477e6200a69d8a926a6b9d099
GFernando97 <angr7991@hotmail.com>
2020-01-03 23:41:34 +0100

    Commented debug comments... files ready to zip again

--- Practica-Final/src/instrucciones.cpp
+++ Practica-Final/src/instrucciones.cpp
@@ -54,13 +54,13 @@ istream &operator>>(istream &i, instrucciones &inst){
     getline(i, linea,'\n');
     istringstream iss(linea);
     while(!iss.eof()){
-      cout << "\n\nNueva linea\n";
+      //cout << "\n\nNueva linea\n";
       getline(iss, nombreAccion, ' ');
       getline(iss, ingredienteAccion1, '\n');
-      cout << "Nombre de la accion: " << nombreAccion << endl;
-      cout << "Nombre del ingrediente: " << ingredienteAccion1 << endl;
-      cout << "Tamaño de la pila antes de interaccion:" << stackOfTrees.size() << endl;
-      cout << "Se ha realizado la accion:\n";
+      //cout << "Nombre de la accion: " << nombreAccion << endl;
+      //cout << "Nombre del ingrediente: " << ingredienteAccion1 << endl;
+      //cout << "Tamaño de la pila antes de interaccion:" << stackOfTrees.size() << endl;
+      //cout << "Se ha realizado la accion:\n";
       if(nombreAccion != ""){
         ariedadAccion = inst.acc.getAriedad(nombreAccion);
         ArbolBinario<string> auxTree(nombreAccion);
@@ -70,20 +70,20 @@ istream &operator>>(istream &i, instrucciones &inst){
             auxTree.Insertar_Hi(auxTree.getRaiz(), ingredienteAccion1);
             stackOfTrees.push(auxTree);
             //cout << "Tamaño de Pila ahora: " << stackOfTrees.size() << endl;
-            cout << "Crear arbol con ingrediente incluido - Ariedad 1\n";
+            //cout << "Crear arbol con ingrediente incluido - Ariedad 1\n";
           }
           else{
             if(!stackOfTrees.empty()){
-              cout << "Voy a usar arboles de la pila por falta de ing...\n";
+              //cout << "Voy a usar arboles de la pila por falta de ing...\n";
               ArbolBinario<string> treeStack = stackOfTrees.top();
               stackOfTrees.pop();
               auxTree.Insertar_Hi(auxTree.getRaiz(), treeStack);
               stackOfTrees.push(auxTree);
               //cout << "Tamaño de Pila ahora: " << stackOfTrees.size() << endl;
-              cout << "Crear arbol con un elemento de la pila - Ariedad 1\n";
+              //cout << "Crear arbol con un elemento de la pila - Ariedad 1\n";
             }
             else{
-              cout << "Error en la pila. Terminar Programa!!!!";
+              //cout << "Error en la pila. Terminar Programa!!!!";
               abort();
             }
           }
@@ -92,10 +92,10 @@ istream &operator>>(istream &i, instrucciones &inst){
         else if(ariedadAccion=="2"){
 
           if(!ingredienteAccion1.empty()){
-            cout << "Interpreto que el ingrediente no esta vacio\n";
+            //cout << "Interpreto que el ingrediente no esta vacio\n";
             istringstream iss2(ingredienteAccion1);
             vector<string> elementosCadena;
-            cout << "Se van a separar los elementos de la cadena de ingredientes.\n";
+            //cout << "Se van a separar los elementos de la cadena de ingredientes.\n";
             while(!iss2.eof()){
               string aux1;
               getline(iss2, aux1, ' ');
@@ -114,14 +114,14 @@ istream &operator>>(istream &i, instrucciones &inst){
                 }
                 else{
                   ingrediente1 = ingrediente1 + " " + elementosCadena[i];
-                  cout << "Ingrediente 1 valores: " << ingrediente1 << endl;
-                  cout << "Contenido en ingredientes: " << inst.containsIngr(ingrediente1) << endl;
+                //cout << "Ingrediente 1 valores: " << ingrediente1 << endl;
+                //cout << "Contenido en ingredientes: " << inst.containsIngr(ingrediente1) << endl;
                 }
 
                 if(inst.containsIngr(ingrediente1)){
                     ingrediente1lleno = true;
-                  //  cout << "Se ha declarado que el ingrediente1 esta lleno\n";
-                  //  cout << "Su valor es:" <<ingrediente1<<endl;
+                  //cout << "Se ha declarado que el ingrediente1 esta lleno\n";
+                  //cout << "Su valor es:" <<ingrediente1<<endl;
                 }
               }
 
@@ -146,12 +146,12 @@ istream &operator>>(istream &i, instrucciones &inst){
             }
 
             if(ingrediente1lleno and ingrediente2lleno){
-               cout << "Ambos ingredientes estan ocupados, por ende se rellenaran sus valores.\n";
-               cout << "Sus valores son: " << ingrediente1 << " Y " << ingrediente2 << endl;
+               //cout << "Ambos ingredientes estan ocupados, por ende se rellenaran sus valores.\n";
+               //cout << "Sus valores son: " << ingrediente1 << " Y " << ingrediente2 << endl;
                auxTree.Insertar_Hi(auxTree.getRaiz(), ingrediente1);
                auxTree.Insertar_Hd(auxTree.getRaiz(), ingrediente2);
                stackOfTrees.push(auxTree);
-               cout << "Se ha creado un arbol con 2 ingredientes dados en la inst - Ariedad 2\n";
+               //cout << "Se ha creado un arbol con 2 ingredientes dados en la inst - Ariedad 2\n";
             }
 
             else{
@@ -161,11 +161,11 @@ istream &operator>>(istream &i, instrucciones &inst){
                 //cout << "Tamaño de Pila ahora: " << stackOfTrees.size() << endl << endl;
                 auxTree.Insertar_Hi(auxTree.getRaiz(), treeStack);
                 stackOfTrees.push(auxTree);
-                cout << "Se ha creado un arbol con un ingrediente y con un arbol de la pila - Ariedad 2\n";
+                //cout << "Se ha creado un arbol con un ingrediente y con un arbol de la pila - Ariedad 2\n";
             }
           }
           else{
-            cout << "Interpreto que el archivo de instrucciones esta vacio\n";
+            //cout << "Interpreto que el archivo de instrucciones esta vacio\n";
             if(!stackOfTrees.empty()){
               ArbolBinario<string> treeStack = stackOfTrees.top();
               stackOfTrees.pop();
@@ -175,7 +175,7 @@ istream &operator>>(istream &i, instrucciones &inst){
               stackOfTrees.pop();
               auxTree.Insertar_Hi(auxTree.getRaiz(), treeStack);
               stackOfTrees.push(auxTree);
-              cout << "Se ha creado un arbol con dos arboles de la pila - Ariedad 2\n";
+              //cout << "Se ha creado un arbol con dos arboles de la pila - Ariedad 2\n";
 
           }
           else{
